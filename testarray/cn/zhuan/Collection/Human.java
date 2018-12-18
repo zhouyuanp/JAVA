9d0b77e570d2e0d07fe7c17ff75499b461353145
zhouyuanp <zyp112490@163.com>
2018-12-18 21:05:36 +0800

    Arraylist

---
+++ testarray/cn/zhuan/Collection/Human.java
@@ -0,0 +1,27 @@
+package cn.zhuan.Collection;
+
+public class Human {
+	private String name;
+
+	public Human(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
